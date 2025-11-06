b44086e70ea07032acdc052eca1c132f00bb6fa6
x341dev <admin@x341.dev>
2025-11-06 12:20:10 +0100

    Fix errors and better handling

--- Program.cs
+++ Program.cs
@@ -7,19 +7,49 @@ namespace Exam
         public static void Main()
         {
             const string MsgInput = "Enter a number (0 to exit): ";
+            const string MsgOverflow = "Number entered too big.";
+            const string MsgNotNumber = "Input is not a number";
+            const string MsgNegative = "Number enteres is not positive";
+            const string MsgError = "Unexpected error ocurred: {0}";
             const string MsgDividers = "The divider numbers of {0} are: ";
             const string MsgExit = "Exiting program";
 
             int number, inc, divisorInt;
             string numstr;
             float divisor;
+            bool valid = false;
+            number = 0;
             do
             {
                 do
                 {
                     Console.Write(MsgInput);
                     numstr = Console.ReadLine();
-                } while (!int.TryParse(numstr, out number));
+
+                    try
+                    {
+                        number = int.Parse(numstr);
+                        if (number < 0)
+                        {
+                            Console.WriteLine(MsgNegative);
+                        } else
+                        {
+                            valid = true;
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine(MsgOverflow);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine(MsgNotNumber);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(MsgError, e);
+                    }
+                } while (!valid);
                 if (number != 0)
                 {
                     Console.Write(MsgDividers, number);
