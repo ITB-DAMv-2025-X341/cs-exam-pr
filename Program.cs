59e0aee77db3fe6e5cd47a97b65f28dbe9cb52a1
x341dev <admin@x341.dev>
2025-11-06 11:56:41 +0100

    Exercici 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Exam
+{
+    class Program
+    {
+        public static void Main()
+        {
+            const string MsgStart = "Starting game!";
+            const string MsgWin = "You win!";
+            const string MsgLose = "You lose.";
+            const string MsgFoundCoin = "You founded a coin. Coins: ";
+            const string MsgFoundTrap = "You falled into a trap. Lives: ";
+
+            int coins = 0;
+            int lives = 3;
+
+            var rand = new Random();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(MsgStart);
+            Console.ResetColor();
+            do
+            {
+                Thread.Sleep(1000);
+                if (rand.Next(1, 3) == 1)
+                {
+                    lives--;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(MsgFoundTrap + lives);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    coins++;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(MsgFoundCoin + coins);
+                    Console.ResetColor();
+                }
+            } while (!(lives == 0 || coins == 5));
+            if (coins == 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(MsgWin);
+                Console.ResetColor();
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(MsgLose);
+                Console.ResetColor();
+            }
+        }
+    }
+}
